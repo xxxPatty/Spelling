6b1d57ce5ef23a27ce3aab86f7db3745fb1d4fdc
Patty <L224749391@gmail.com>
2021-03-28 14:39:08 +0800

    Initial Commit

---
+++ Spelling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spelling
+//
+//  Created by 林湘羚 on 2021/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spelling/SpellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingApp.swift
+//  Spelling
+//
+//  Created by 林湘羚 on 2021/3/28.
+//
+
+import SwiftUI
+
+@main
+struct SpellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
